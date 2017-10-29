62ae466a3e41a7947c512ab3459d8530fc8d968f
Isak Mladenoff <imladenoff@gmail.com>
2017-10-28 17:40:56 -0700

    Add routes

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 var db = require('./model/db'),
     blob = require('./model/blobs');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index');
+var blobs = require('./routes/blobs');
 
 var app = express();
 
@@ -24,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', routes);
+app.use('/blobs', blobs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
